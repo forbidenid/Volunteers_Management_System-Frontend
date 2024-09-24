54ee4ac6d40e58c9d3e3e1ade6c3d94dfee4ee04
forbidenid <muriithidennnis340@gmail.com>
2024-09-24 15:12:10 +0300

    app main file

---
+++ vms_backend/app.py
@@ -0,0 +1,37 @@
+from categories.models import Category
+from comments.models import Comment
+from api.factory import app, db
+from file_uploads.models import FileUpload, GigImage, TagImage, CategoryImage
+from orders.models import Order
+from gigs.models import Product
+from routes import blueprint
+from tags.models import Tag
+from users.models import User
+
+# Extensions, it is not how a well organized project initializes the extensions but hey, it
+# is simple and readable anyways.
+
+
+app.register_blueprint(blueprint, url_prefix='/api')
+
+
+# Like the old school Flask-Script for the shell, but using the new Flask CLI which is way better
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User, gig=Gig,
+                tag=Tag, category=Category, comment=Comment, file_upload=FileUpload, tag_image=TagImage,
+                category_image=CategoryImage, gig_image=GigImage)
+
+
+'''
+Not used, to seed the database, it is as easy as running the seed_database.py python script
+import click
+import sys
+@app.cli.command()
+@click.option('--seed', default=None, help='seed the database')
+def seed_db(value):
+    sys.stdout.write('seed the database')
+'''
+
+if __name__ == '__main__':
+    app.run(port=8080)
